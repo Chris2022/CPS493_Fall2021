5fffd64aca7e326463b460f090c15c294ae7b9b2
Chris2022 <ctejada415gmail.com>
2021-09-27 14:11:32 -0400

    index js file

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
-const http = require('http');
+const express = require('express')//can include express because of the node module 
+const app = express() //need to call the express constructor right away
+const port = 3000
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// app is an object which will have many things attached.
+// when someone sends a get message with this particular path,
+// then I will send back hello world
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+.get("/newpaltz", (req,res) => {
+  res.send("Hello Newpaltz")
+})
 
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
