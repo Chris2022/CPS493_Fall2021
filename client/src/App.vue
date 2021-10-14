58afd7fafde239025de5928502bfe42b20fcedb8
Chris2022 <ctejada415gmail.com>
2021-10-14 14:30:13 -0400

    added routing links and details

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="container">
-        <Nav />
+        <div class="columns">
+            <div class="column">
 
+            </div>
+        </div>
+        <Nav />
         <router-view/>        
     </div>
-
 </template>
 
 <style lang="scss">
