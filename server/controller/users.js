6d41cd0ae44e229ac405ccdd2a23bcd12ded84ce
Chris2022 <ctejada415gmail.com>
2021-10-21 15:34:36 -0400

    Big update

--- server/controller/users.js
+++ server/controller/users.js
@@ -3,8 +3,8 @@ const model = require("../model/users")
 const app = express.Router() //creates a new pipeline
 
 app
-    .get('/',(req,res,next) => {
-        res.send([{name:"Chris"}]);
-    })
+.get("/", (req, res, next) =>{
+    res.send(model.GetAll());
+})
 
 module.exports = app;// the export of this file is its own app. 
