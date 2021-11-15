dc01c0d6ede1326348d7448a81897cf30bae5061
Chris2022 <ctejada415gmail.com>
2021-11-15 14:31:24 -0500

    error handling

--- client/src/services/myFetch.js
+++ client/src/services/myFetch.js
@@ -1,6 +1,14 @@
+import session from "./session";
+
 const API_ROOT = process.env.VUE_APP_API_ROOT ?? 'http://localhost:3100';
 
-export function api(url){
-    return fetch(API_ROOT + url).then(x=>x.json());
+ export async function api(url){
+     try {
+        // return fetch(API_ROOT + url).then(x=>x.json()).catch(err => session.error(err));
+        const x = await fetch(API_ROOT + url);
+        return await x.json();
+     } catch (err) {
+         session.error(err);
+     }
 }
 
--- client/src/services/session.js
+++ client/src/services/session.js
@@ -12,16 +12,21 @@ const session = {
             this.user = response.user;
             router.push(this.toRoute);
         } catch (error) {
-            this.messages.push({text:error.msg, type:'warning'});
-            NotificationProgrammatic.open({
-                duration:5000,
-                message: error.msg,
-                variant: 'danger',
-                closable: true,
-                type: 'danger',
-
-            })
+            this.error(error)
         }
+    },
+    error(error){
+        console.log(error);
+        const msg  = error.msg ?? error;
+        this.messages.push({text:msg, type:'warning'});
+        NotificationProgrammatic.open({
+            duration:5000,
+            message: error.msg,
+            variant: 'danger',
+            closable: true,
+            type: 'danger',
+
+        })
     }
 };
 
