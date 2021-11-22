fa878660f9df0dd652f3ac343753ace45e530839
Chris2022 <ctejada415gmail.com>
2021-11-22 14:09:31 -0500

    updates

--- client/src/services/session.js
+++ client/src/services/session.js
@@ -1,23 +1,28 @@
 import router from "../router";
 import { Login } from "./users";
-import {NotificationProgrammatic} from "@oruga-ui/oruga-next/dist/esm/notification";
+import { NotificationProgrammatic } from "@oruga-ui/oruga-next/dist/esm/notification";
 
 const session = {
     user: null,
-    messages: [],//{text:string, type:string}
-    toRoute: "/feed",
-    Login(handle, password){
+    messages: [],       // {text: string, type: string }
+    toRoute: '/feed',
+    async Login(handle, password){
+
         try {
-            const response = Login(handle,password);
+            const response = await Login(handle, password);
+
             this.user = response.user;
+    
             router.push(this.toRoute);
+                
         } catch (error) {
-            this.Error(error)
+            this.Error(error);
         }
     },
     Error(error){
         console.error(error);
         const msg = error.msg ?? error;
+
         this.messages.push({ text: msg, type: 'warning' })
         NotificationProgrammatic.open({
             duration: 5000,
